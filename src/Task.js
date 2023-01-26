339e77bf32a82cc3f42ea9b84fa5938726635ef9
murat-dede <murat1_dede@hotmail.com>
2023-01-26 14:55:57 +0300

    s9g2
    Task js 19. satır renk değiştiremedim.

--- src/Task.js
+++ src/Task.js
@@ -1,11 +1,22 @@
 import React from "react";
+import{
+  differenceInDays,
+  formatDistanceToNow,
+  formatDistanceToNowStrict,
+} from "date-fns";
+import { tr } from "date-fns/locale";
+
 
 const Task = ({ taskObj, onComplete }) => {
-
+  const kalanGun = formatDistanceToNow(new Date(taskObj.deadline),{
+  locale: tr,
+ 
+});
+const test = kalanGun<=3 ? true : false;
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+      <div className="deadline">son teslim: <span className= /*kalanGun <= 3 ?*/ "input-error">{kalanGun}</span></div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
